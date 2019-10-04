c4bade3d5b92c641b44784767745038c93cb784c
bundickm <bundickm@gmail.com>
2019-10-04 08:36:59 -0700

    Sprint Challenge complete, working on stretch now

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __repr__(self):
+    return (f'{self.name} {self.lat}, {self.lon}')
+
+  def __str__(self):
+    return(f'Name: {self.name}\nLatitude: {self.lat}\nLongitude: {self.lon}')
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -17,17 +27,30 @@
 cities = []
 
 def cityreader(cities=[]):
+  import csv
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+  with open('cities.csv', newline='') as csvfile:
+    city_reader = csv.reader(csvfile)
+
+    # Ignore the header
+    next(city_reader)
+
+    for city in city_reader:
+      name = city[0]
+      lat = float(city[3])
+      lon = float(city[4])
+
+      cities.append(City(name, lat, lon))
+
+  return cities
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-    print(c)
+  print(c)
 
 # STRETCH GOAL!
 #
